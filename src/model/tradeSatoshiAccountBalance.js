812e41419792e83d9e1c89d0727b2d524aa87e8f
Justin Schellenberg <jrschellenberg@gmail.com>
2018-02-01 21:02:08 -0600

    Refactored setAccountBalance to only show ones in account.

--- src/model/tradeSatoshiAccountBalance.js
+++ src/model/tradeSatoshiAccountBalance.js
@@ -1,11 +1,16 @@
 export default class TradeSatoshiCurrencies {
-		static balance = [];
+		static balance = {};
 		
 	static async getAccountBalance() {
 		return await TradeSatoshiCurrencies.balance;
 	}
 	
 	static async setAccountBalance(coins) {
-		TradeSatoshiCurrencies.balance = coins;
+		for(let i=0; i<coins.length; i++){
+			if(coins[i].total != 0){
+				let key = coins[i].currency;
+				TradeSatoshiCurrencies.balance[key] = coins[i].total;
+			}
+		}
 	}
 }
