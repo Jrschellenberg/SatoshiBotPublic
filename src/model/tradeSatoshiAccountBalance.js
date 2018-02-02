849f89ce3a7619bc0ec8ab25e77102d0f4fdd1f4
Justin Schellenberg <jrschellenberg@gmail.com>
2018-02-01 20:56:55 -0600

    Committing changes

--- src/model/tradeSatoshiAccountBalance.js
+++ src/model/tradeSatoshiAccountBalance.js
@@ -1,21 +1,11 @@
-
-
-class TradeSatoshiCurrencies {
+export default class TradeSatoshiCurrencies {
 		static balance = [];
-	
+		
 	static async getAccountBalance() {
 		return await TradeSatoshiCurrencies.balance;
 	}
 	
 	static async setAccountBalance(coins) {
-		for(let i=0; i<coins.length; i++){
-			await TradeSatoshiCurrencies.balance.push(coins[i]);
-		}
-		//console.log(TradeSatoshiCurrencies.balance);
+		TradeSatoshiCurrencies.balance = coins;
 	}
 }
-
-
-
-
-export default TradeSatoshiCurrencies;
