69b36b5e6cabce7f8d7eb2b26a0c5e75e6521feb
Justin Schellenberg <jrschellenberg@gmail.com>
2018-02-02 13:57:48 -0600

    bit refactoring

--- src/controller/SatoshiTradeScout.js
+++ src/controller/SatoshiTradeScout.js
@@ -24,18 +24,10 @@ export default class SatoshiTradeScout {
 	}
 	
 	static getWork(slaveNumber){
-		if(SatoshiTradeScout.slaveIndexs[slaveNumber] == SatoshiTradeScout.mappedMarketPairings[slaveNumber].length){
-			console.log("Abuot to hit this?!?!");
+		if(SatoshiTradeScout.slaveIndexs[slaveNumber] === SatoshiTradeScout.mappedMarketPairings[slaveNumber].length){
 			SatoshiTradeScout.slaveIndexs[slaveNumber] = 0;
-			console.log("Is it hitting this?!?");
 			return SatoshiTradeScout.mappedMarketPairings[slaveNumber][SatoshiTradeScout.slaveIndexs[slaveNumber]++]; // I think it errroring here!!!!!
 		}
-		
-		let index = SatoshiTradeScout.slaveIndexs[slaveNumber];
-		console.log(index);
-		console.log(SatoshiTradeScout.mappedMarketPairings[slaveNumber].length);
-		
-		//console.log(SatoshiTradeScout.mappedMarketPairings[slaveNumber][++SatoshiTradeScout.slaveIndexs[slaveNumber]]);
 		return SatoshiTradeScout.mappedMarketPairings[slaveNumber][SatoshiTradeScout.slaveIndexs[slaveNumber]++]; //Increment index by 1 and send off work.
 	}
 }
