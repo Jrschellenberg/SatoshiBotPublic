872c6e68076a3b6973353ed0cf61c14fdd3ddb78
Justin Schellenberg <jrschellenberg@gmail.com>
2018-02-10 14:20:52 -0600

    Started writing test for utilities

---
+++ test/main_test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+
+describe("Mocha", () => {
+	//Test Spec (Unit test)
+	it('should run our tests using npm', () => {
+		expect(true).to.be.ok;
+	});
+	
+});
---
+++ test/utilities_test.js
@@ -0,0 +1,35 @@
+import Utilities from '../src/utilities';
+
+const expect = require('chai').expect;
+
+
+describe('precisionRound', () => {
+	let utilities = new Utilities();
+	
+	if('should return NaN if supplied with non integer arguments', ()=> {
+			expect(utilities.precisionRound('stringOne', 0.12312312312)).to.be.NaN;
+			expect(utilities.precisionRound(0.123, 'stringTwo')).to.be.NaN;
+			expect(utilities.precisionRound(null, 'stringTwo')).to.be.NaN;
+			expect(utilities.precisionRound(12.23, 'stringTwo')).to.be.NaN;
+			expect(utilities.precisionRound(12.23, 'stringTwo')).to.be.NaN;
+		});
+	
+	it('should return 0 if supplied with null arguments', () => {
+		expect(utilities.precisionRound(null, null)).to.be.equal(0);
+	});
+	
+	it('should return 0 if first  argument is null', () => {
+		expect(utilities.precisionRound(null, 10)).to.be.equal(0);
+	});
+	it('should round to nearest decimal place if second argument is null', ()=>{
+		expect(utilities.precisionRound(10.12, null)).to.be.equal(10);
+		expect(utilities.precisionRound(10.62, null)).to.be.equal(11);
+	});
+	
+	it('should return a first argument rounded to number decimal places specified by second argument', () => {
+		expect(utilities.precisionRound(20.1234532, 2)).to.be.equal(20.12);
+		expect(utilities.precisionRound(20.1274532, 2)).to.be.equal(20.13);
+		expect(utilities.precisionRound(20.1274532234235, 8)).to.be.equal(20.12745322);
+	});
+	
+});
