6927802cd87f5cb5bde457591f9725e19b9a893f
Justin Schellenberg <jrschellenberg@gmail.com>
2018-02-10 22:48:25 -0600

    Finished Tests on cryptopiaMiddleware

--- src/middleware/cryptopiaMiddleware.js
+++ src/middleware/cryptopiaMiddleware.js
@@ -40,21 +40,13 @@ export class CryptopiaMiddleware extends TradeMiddleware {
 	}
 	
 	checkMinimumTrades(markets, currencies ){
-		//console.log("Got into check Minimum.");
 		let marketOneTrade = markets[0].rate * markets[0].quantity,
 			marketTwoTrade = markets[1].rate * markets[1].quantity,
 			marketThreeTrade = markets[2].rate * markets[2].quantity,
-		currencyOne = currencies[0],
 		currencyTwo = currencies[1],
 		currencyThree = currencies[2];
 		let passedChecks = [false, false];
-		
-		// console.log(`currencyThree is ${currencyThree}`);
-		// console.log(`currencyTwo is ${currencyTwo}`);
-		// console.log(`marketOneTrade is ${marketOneTrade}`);
-		// console.log(`marketTwoTrade is ${marketTwoTrade}`);
-		// console.log(`marketThreeTrade is ${marketThreeTrade}`);
-		
+				
 		if(super.isUSDT(currencyThree) || super.isNZDT(currencyThree)){
 			if(marketOneTrade > 1.00 && marketTwoTrade > 1.00){
 				passedChecks[0] = true;
@@ -75,12 +67,10 @@ export class CryptopiaMiddleware extends TradeMiddleware {
 				passedChecks[1] = true;
 			}
 		}
-		//console.log(`passed checks are ${passedChecks[0]} as well as ${passedChecks[1]}`);
 		return passedChecks[0] && passedChecks[1];
-		
 	}
-	
 }
+
 let instance = null;
 export class CryptopiaCurrencies {
 	
