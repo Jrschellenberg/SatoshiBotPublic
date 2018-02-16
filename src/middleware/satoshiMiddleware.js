741b1b109327dc457f83033b6ba9c8355c3811dd
Justin Schellenberg <jrschellenberg@gmail.com>
2018-02-16 14:34:25 -0600

    fixed more broken unit tests

--- src/middleware/satoshiMiddleware.js
+++ src/middleware/satoshiMiddleware.js
@@ -6,46 +6,83 @@ export class SatoshiMiddleware extends TradeMiddleware {
 	}
 	
 	async getMarketListing(params){
-		 const market = await this.service.getOrderBook(params);
-		 return market;		
-	}
-	
-	checkMinimumTrades(markets, currencies){
-		
-	}
-}
-
-export class TradeSatoshiCurrencies {
-	static balance = {};
-	static profit = 0;
-	
-	static async getAccountBalance() {
-		return await TradeSatoshiCurrencies.balance;
-	}
-	
-	static async setAccountBalance(coins) {
-		for(let i=0; i<coins.length; i++){
-			if(coins[i].total != 0){
-				let key = coins[i].currency;
-				TradeSatoshiCurrencies.balance[key] = coins[i].total;
+		let newParams = {
+			market: params.market,
+			depth: params.depth
+		};
+		const market = await this.service.getOrderBook(newParams);
+		try{
+			if(market.buy){
+				let marketObject = {
+					buy: [
+						{
+							quantity: market.buy[0].quantity,
+							rate: market.buy[0].rate
+						}
+					],
+					sell: [
+						{
+							quantity: market.sell[0].quantity,
+							rate: market.sell[0].quantity
+						}
+					]
+				};
+				return marketObject;
+			}
+			else{
+				return null;
 			}
 		}
-	}
-	static tallyProfitableTrade(amount){
-		TradeSatoshiCurrencies.profit += amount;
-	}
-	
-	static async getBalances(){
-		const balance = await TradeSatoshiCurrencies.getAccountBalance();
-		return balance;
+		catch (err) {
+			//throw new TypeError("Error in getMarketListing function!!!"+err);
+			//console.log("hitting the catch here!!");
+			console.log("Error in getMarketListing function!!!");
+			console.log(err);
+		}
 	}
 	
-	static async setBalances(service){
-		const getBalances = await service.getBalances();
-		console.log("we getting back over here?");
-		await TradeSatoshiCurrencies.setAccountBalance(getBalances);
+	
+	checkMinimumTrades(markets, currencies) {
+		let marketOneTrade = markets[0].rate * markets[0].quantity,
+			marketTwoTrade = markets[1].rate * markets[1].quantity,
+			marketThreeTrade = markets[2].rate * markets[2].quantity,
+			currencyTwo = currencies[1],
+			currencyThree = currencies[2];
+		let passedChecks = [false, false];
+		
+		if (super.isUSDT(currencyThree) || super.isBTC(currencyThree) || super.isLTC(currencyThree) || super.isDOGE(currencyThree) || super.isBCH(currencyThree)) {
+			if (marketOneTrade > 0.000005 && marketTwoTrade > 0.000005) {
+				passedChecks[0] = true;
+			}
+		}
+		if (super.isUSDT(currencyTwo) || super.isBTC(currencyTwo) || super.isLTC(currencyTwo) || super.isDOGE(currencyTwo) || super.isBCH(currencyTwo)) {
+			if (marketThreeTrade > 0.000005) {
+				passedChecks[1] = true;
+			}
+		}
+		return passedChecks[0] && passedChecks[1];
 	}
+	
+	
+	
+}
 
+let instance = null;
+
+export class TradeSatoshiCurrencies {
+	constructor(service) {
+		if (instance) return instance;
+		this.balance = {};
+		this.service = service;
+		instance = this;
+	}
+	
+	async setBalances() {
+		const getBalances = await this.service.getBalances();
+		console.log("we getting back over to setting balances?");
+		//await this.setAccountBalance(getBalances);
+	}
+	
 	
 	
 }
